2183dd19693775e7ded7ced9e3944e3d8e7f34d9
David Ludwig <dludwig@pobox.com>
2014-01-11 23:43:20 -0500

    turned off peg antialiasing

--- FallingStuff2Saver/Scene.m
+++ FallingStuff2Saver/Scene.m
@@ -114,6 +114,8 @@ static CGRect CGRectCentered(CGFloat cx, CGFloat cy, CGFloat width, CGFloat heig
 		peg.fillColor = [(SKColor *)RandArrayElement(C_OBJ("peg-background-colors")) colorWithAlphaComponent:C_FLOAT("peg-background-alpha")];
 		peg.strokeColor = [SKColor whiteColor];
 		peg.glowWidth = 0.0;
+		peg.lineWidth = 1.0;
+		peg.antialiased = NO;
 		[self addChild:peg];
 	}
 }
